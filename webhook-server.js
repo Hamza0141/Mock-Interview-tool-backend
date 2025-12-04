a28bfa68b5c5327d0ddd673710a5d5002c3679c4
Hamza0141 <131574624+Hamza0141@users.noreply.github.com>
2025-12-03 21:25:44 -0600

    payment getway updated

--- webhook-server.js
+++
@@ -1,9 +0,0 @@
-// // webhook-server.js
-// require("dotenv").config();
-// const webhookApp = require("./webhook/webhookApp");
-
-// const WEBHOOK_PORT = process.env.WEBHOOK_PORT;
-
-// webhookApp.listen(WEBHOOK_PORT, () => {
-//   console.log(`Stripe webhook server running on port ${WEBHOOK_PORT}`);
-// });
